9a82673aa72bf5a8b4e67536c4a44fcd6e97fc91
Yurij <horyuriy@gmail.com>
2023-08-04 15:47:00 +0300

    add binary

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public static class BinarySearch
+    {
+        public static int SearchBinary(int[] array, int searchedValue, int first, int last)
+        {
+            if (first > last)
+            {
+                return -1;
+            }
+            var middle = (first + last) / 2;
+            var middleValue = array[middle];
+            if(middleValue == searchedValue)
+            {
+                return array[middleValue];
+            }
+            else
+            {
+                if( middleValue >searchedValue)
+                {
+                    return SearchBinary(array, searchedValue, first, middle-1);
+                }
+                else
+                {
+                    return SearchBinary(array, searchedValue, middle + 1, last);
+                }
+            }
+        }
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary search (recursive implementation)");
+            Console.WriteLine("Enter the elements of array");
+            var s = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            var array = new int[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                array[i] = Convert.ToInt32(s[i]);
+            }
+            Array.Sort(array);
+            Console.WriteLine("Sorted array: {0}", string.Join(", ", array));
+            while(true)
+            {
+                Console.WriteLine("Enter the desired value or -1 for the output");
+                var k = Convert.ToInt32(Console.ReadLine());
+                if (k == -1)
+                {
+                    break;
+                }
+                var searchResult = BinarySearch.SearchBinary(array, k, 0, array.Length - 1);
+                if(searchResult < 0)
+                {
+                    Console.WriteLine("Element with value {0} not found", k);
+                }
+                else
+                {
+                    Console.WriteLine("The element is found. The index of an element with the value {0} is equal to {1}", k, searchResult);
+                }
+            }
+
+        }
+    }
+}
